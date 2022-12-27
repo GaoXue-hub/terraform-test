646b9bad38b7b6f312479aba632dfff69b4f9b41
GaoXue-hub <117557748+GaoXue-hub@users.noreply.github.com>
2022-12-27 18:37:15 +0900

    main.tf

---
+++ main.tf
@@ -0,0 +1,33 @@
+provider "aws" {
+  region = "ap-northeast-1"
+  access_key = ""
+  secret_key = ""
+}
+
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+  backend "remote" {
+    hostname = "app.terraform.io"
+    organization = "GAOXUE"
+    workspaces {
+      name="desktop-tutorial"
+    }
+    token = "ygyIiXMgLicstw.atlasv1.KYesbOEtXVl9KlkJ8C9nfzgWCMzlGHFzaqOgqImAhnlaQtH0HN2ShvTWT0W5HRw2abI"
+  }
+}
+
+resource "aws_vpc" "terraform-vpc" {
+  cidr_block           = "10.0.0.0/16"
+  instance_tenancy     = "default"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "terraform-vpc"
+  }
+}
